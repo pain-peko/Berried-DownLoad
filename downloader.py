ab2080bcfbd63476c1c4a6903369560cc8f91192
VSorokin <pan.vladsorokin@gmail.com>
2024-04-11 21:21:21 +0200

    v. 1.03

---
+++ downloader.py
@@ -0,0 +1,214 @@
+## TODO:
+# 7. make cool executable, that makes windows shortcut and stuff
+# 8. Transfer subs into folder??
+# 9. make all pathes relative
+# 10. Change yt-dlp path added when installing the program
+
+## comments
+#'--embed-subs',
+#'--convert-subs',
+#'ass',
+#'en.*,ja.*',
+#'--sub-format',
+#'ass/srt/best',
+#'--write-comments'
+
+import os
+import subprocess
+import sys
+from termcolor import colored
+from datetime import datetime
+from pathlib import Path
+from logbox import Logbox
+
+
+
+# ---------------------------------------------------------------------------- #
+#                                    Consts                                    #
+# ---------------------------------------------------------------------------- #
+DETAILED_DEBUG = True
+PATH_ROOT = os.path.dirname(os.path.realpath(__file__))
+PATH_DL = os.path.join(PATH_ROOT, "downloads")
+YT_DLP_PATH = "D:\Soft\yt-dlp\yt-dlp.exe" 
+
+
+
+# ---------------------------------------------------------------------------- #
+#                                    Command                                   #
+# ---------------------------------------------------------------------------- #
+
+def audio_cmd(cmd):
+    # Formatting
+    cmd.extend(['-f', 'bestaudio', '-x'])
+
+def video_cmd(cmd):
+    # Formatting
+    cmd.extend(['-f', 'bestvideo*+bestaudio/best'])
+
+    # Subs
+    cmd.append('--write-sub')
+    cmd.append('--sub-langs')
+    cmd.append('all')
+
+    # remux to matroska (if video)]
+    cmd.append('--remux-video')
+    cmd.append('mkv')
+
+    # chapters
+    cmd.append('--embed-chapters')
+
+def create_cmd(media_type, repeated_flag, url, dl_dir):
+
+    if (media_type != 'video' and media_type != 'audio'):
+        print(colored('func create_cmd, media_type must be "video" or "audio"', 'red'))
+        sys.exit()
+
+    # Base
+    cmd = [YT_DLP_PATH, '-o', f'{dl_dir}\%(title)s\%(title)s.%(ext)s']
+
+    # Debugging
+    if DETAILED_DEBUG:
+        cmd.append('--verbose')
+
+    # Media type specific
+    if (media_type == 'video'):
+        video_cmd(cmd)
+    else:
+        audio_cmd(cmd)
+
+    if repeated_flag == False:
+        # Pull info
+        cmd.extend(['--write-thumbnail',
+                    '--write-description',
+                    '--write-info-json',
+                    '--write-annotations',
+                    '--write-url-link'])
+
+    # post-processing
+    cmd.extend(['--convert-thumbnails', 'png'])
+    cmd.append('--embed-thumbnail')
+    cmd.append('--embed-metadata')
+
+    # URL
+    cmd.append(url)
+
+    return cmd
+
+def show_list_cmd(url):
+    cmd = [YT_DLP_PATH, '--list-formats', url]
+    if DETAILED_DEBUG:
+        cmd.append('--verbose')
+    
+    return cmd
+
+
+
+# ---------------------------------------------------------------------------- #
+#                                    Execute                                   #
+# ---------------------------------------------------------------------------- #
+def execute(cmd, si):
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, startupinfo=si)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line 
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
+
+
+
+# ---------------------------------------------------------------------------- #
+#                                     Main                                     #
+# ---------------------------------------------------------------------------- #
+def main(media_type, url, dl_dir, textbox=False):
+
+    # hide yt-dlp windows
+    si = subprocess.STARTUPINFO()
+    si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+    # Open log file
+    now = datetime.now()
+    now = now.replace(microsecond=0)
+    now = str(now).replace(':', '-')
+    now = now.split(' ')
+    if os.path.basename(PATH_ROOT) == 'Data':
+        log_dir_path = f'{os.path.dirname(PATH_ROOT)}\\logs'
+    else:
+        log_dir_path = f'{PATH_ROOT}\\logs'
+    Path(log_dir_path).mkdir(parents=True, exist_ok=True)
+    log_file = f'{log_dir_path}\\[{now[0]}] [{now[1]}] example_name.log'
+    f = open(log_file, "w")
+
+    # Available formats
+    f.write('\n\n\n\n\n')
+    for path in execute(show_list_cmd(url), si):
+        f.write(path)
+        Logbox.dump(textbox, path)
+    Logbox.dump(textbox, 'info_over')
+    Logbox.dump(textbox, 'newline')
+
+    # Download chose format
+    Logbox.dump(textbox, 'dl_start')
+    f.write('\n\n\n')
+    if (media_type != 'both'):
+        for path in execute(create_cmd(media_type, False, url, dl_dir), si):
+            f.write(path)
+            Logbox.dump(textbox, path)
+    else:
+        for path in execute(create_cmd('audio', True, url, dl_dir), si):
+            f.write(path)
+            Logbox.dump(textbox, path)
+        Logbox.dump(textbox, 'newline')
+        for path in execute(create_cmd('video', False, url, dl_dir), si):
+            f.write(path)
+            Logbox.dump(textbox, path)   
+    Logbox.dump(textbox, 'newline')
+    # Close log
+    f.close()
+
+    # Rename log
+    name_found = False
+    for _ in (True,):
+        with open(log_file, 'r') as filedata:
+            for line in filedata:
+                if (f"to: {dl_dir}" in line.replace('\\', '/')):
+                    result = line.replace('\\', '/').split(dl_dir)
+                    title = result[1].split('/')
+                    title = title[1]
+                    name_found = True
+                    break
+
+    new_log_file = log_file
+    if name_found:
+        new_log_file = f'{log_dir_path}\\[{now[0]}] [{now[1]}] {title}.log'
+        os.rename(log_file, new_log_file)
+
+    Logbox.dump(textbox, f'Download finished, detailed log saved to: {new_log_file}')
+
+
+if __name__ == "__main__":
+    # Handle arg URL 
+    ERROR = 0
+    if len(sys.argv) != 3:
+        ERROR = 1
+        
+    if (sys.argv[1] != 'video' and sys.argv[1] != 'audio' and sys.argv[1] != 'both'):
+        ERROR = 1
+
+    if ERROR == 1:
+        print(colored("Function is called wrong", 'red'))
+        print(colored('You must use the form: python main.py media_type https://www.example_url.com\nmedia_type can be "audio", "video" or "both" (without quotes)', 'red'))
+        sys.exit()
+
+    if sys.argv[1] == 'video':
+        MEDIA_TYPE = 'video'
+    elif sys.argv[1] == 'audio':
+        MEDIA_TYPE = 'audio'
+    elif sys.argv[1] == 'both':
+        MEDIA_TYPE = 'both'
+
+    URL = sys.argv[2]
+
+    Path(PATH_DL).mkdir(parents=True, exist_ok=True) # create folder if didn't exist already
+    main(MEDIA_TYPE, URL, PATH_DL)
+
---
+++ gui.py
@@ -0,0 +1,224 @@
+##TODO:
+# 2. examples: https://github.com/TomSchimansky/CustomTkinter/tree/master/examples
+# 3. docs: https://customtkinter.tomschimansky.com/documentation/
+# 11. fix japanese characters
+# 16. put ffmpeg folder inside _internal and then in installer add path to this folder, but ask user if they want it
+
+
+import os
+import customtkinter
+from PIL import Image
+import json
+from pathlib import Path
+import jsonpickle
+import downloader as BerriedDL
+import threading
+
+
+# ---------------------------------------------------------------------------- #
+#                                    Consts                                    #
+# ---------------------------------------------------------------------------- #
+PATH_ROOT = os.path.dirname(os.path.realpath(__file__))
+PATH_CONFIG = os.path.join(PATH_ROOT, "config")
+Path(PATH_CONFIG).mkdir(parents=True, exist_ok=True) # create folder if didn't exist already
+
+RADIO_NAMES = ['Video', 'Only Audio', 'Both Types']
+LABEL_NAMES = ['YouTube URL:', 'Media Type:', 'Download Path:']
+
+
+# ---------------------------------------------------------------------------- #
+#                                     Theme                                    #
+# ---------------------------------------------------------------------------- #
+
+#customtkinter.set_default_color_theme("green")
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme(os.path.join(PATH_CONFIG, "custom_theme.json"))
+
+
+# ---------------------------------------------------------------------------- #
+#                            Frame for all settings                            #
+# ---------------------------------------------------------------------------- #
+class Settings():
+    def __init__(self):
+        # default values
+        self.radio_state = customtkinter.StringVar(value=RADIO_NAMES[0])
+        if os.path.basename(PATH_ROOT) == 'Data':
+            self.dlpath = os.path.join(os.path.dirname(PATH_ROOT), "downloads")
+        else:
+            self.dlpath = os.path.join(PATH_ROOT, "downloads")
+
+        # load json
+        settings_path = os.path.join(PATH_CONFIG, "settings.json")
+        if (Path(settings_path).is_file()):
+            with open(settings_path, "r") as file:
+                data = json.load(file)
+            obj = jsonpickle.decode(data)
+            self.radio_state = customtkinter.StringVar(value=obj.radio_state)
+            self.dlpath = obj.dlpath
+    
+    def preJSON(self):
+        self.radio_state = self.radio_state.get()
+
+
+class SettingsFrame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        # Values
+        self.settings = Settings()
+        self.radios = RADIO_NAMES
+        self.labels = LABEL_NAMES
+        self.radiobuttons = []
+
+        # Main Frame Grid 3x2
+        self.grid_columnconfigure(1, weight=1)
+
+        # Labels
+        for i, value in enumerate(self.labels):
+            label = customtkinter.CTkLabel(self, text=value)
+            label.grid(row=i, column=0, padx=20, pady=20, sticky="w")
+
+        # Entry
+        self.entry_field = customtkinter.CTkEntry(self, placeholder_text="https://www.youtube.com/", width=5000)
+        self.entry_field.grid(row=0, column=1, padx=20, pady=20, sticky="w")
+
+        # Radiobuttons
+        buttons_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        buttons_frame.grid(row=1, column=1, sticky="w")
+        
+        for i, value in enumerate(self.radios):
+            radiobutton = customtkinter.CTkRadioButton(buttons_frame, text=value, value=value, variable=self.settings.radio_state)
+            radiobutton.grid(row=0, column=i, padx=20, pady=20, sticky="ws")
+            self.radiobuttons.append(radiobutton)
+        
+        # Browse
+        browse_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        browse_frame.grid(row=2, column=1, sticky="w")
+        browse_frame.grid_columnconfigure(0, weight=1)
+
+        self.entry_browse = customtkinter.CTkEntry(browse_frame, textvariable=customtkinter.StringVar(value=self.settings.dlpath), width=5000)
+        self.entry_browse.grid(row=0, column=0, padx=20, pady=20, sticky="w")
+
+        browse_button = customtkinter.CTkButton(browse_frame, text="Browse", command=lambda: self.browsefunc(self.entry_browse), font=("Roboto", 14))
+        browse_button.grid(row=0, column=1, padx=20, pady=20, sticky="w")
+
+    def browsefunc(self, entry):
+        filename = customtkinter.filedialog.askdirectory()
+        if filename:
+            entry.configure(textvariable=customtkinter.StringVar(value=filename))
+
+
+# ---------------------------------------------------------------------------- #
+#                                  Main Object                                 #
+# ---------------------------------------------------------------------------- #
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # Window
+        self.title("Berried DL v.1.03 - reclaimer of my yt videos")
+        self.iconbitmap(f'{os.path.join(PATH_ROOT, "images")}//icon3.ico')
+        self.geometry(f"{1050}x{800}")
+        self.minsize(900,340)
+
+        # Grid
+        self.grid_columnconfigure(0, weight=0)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(2, weight=1)
+
+        # Picture
+        image_path = os.path.join(PATH_ROOT, "images")
+        self.img = customtkinter.CTkImage(Image.open(os.path.join(image_path, "label4.png")), size=(250, 300))
+        
+        self.corp_pic_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.corp_pic_frame.grid(row=0, column=0, sticky="nw", rowspan=2)
+
+        self.corp_pic_frame_label = customtkinter.CTkLabel(self.corp_pic_frame, text="", image=self.img)
+        self.corp_pic_frame_label.grid(row=0, column=0, padx=20, pady=20)
+
+        self.settings_frame = SettingsFrame(self)
+        self.settings_frame.grid(row=0, column=1, padx=25, pady=(25,0), sticky="nwe")
+
+        # Start Button
+        self.button = customtkinter.CTkButton(self, text="Start", command=self.button_callback_threaded, height=40, font=("Helvetica", 17, 'bold'), fg_color='#496F4E', hover_color='#38563D')
+        self.button.grid(row=1, column=1, padx=10, pady=0, sticky="n") #, columnspan=2
+
+        # Logs
+        self.log_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.log_frame.grid(row=2, column=0, sticky="nsew", columnspan=2)
+        self.log_frame.grid_columnconfigure(0, weight=1)
+        self.log_frame.grid_rowconfigure(0, weight=1)
+
+        self.textbox = customtkinter.CTkTextbox(self.log_frame, width=5000, height=5000, corner_radius=0, wrap=customtkinter.WORD, font=("Consolas", 14), fg_color="#181818") #, state="disabled"
+        self.textbox.grid(row=0, column=0, sticky="nsew", padx=25, pady=25)
+
+        # tags
+        # API was changed to allow font in tags
+        self.textbox.tag_config('info_label', font=("Consolas", 14), foregroun="#159acd")
+        self.textbox.tag_config('highlight1', foregroun="#f5f543") # yellow
+        self.textbox.tag_config('highlight2', foregroun="#7F7F7F") # grey
+        self.textbox.tag_config('highlight3', font=("Consolas", 14), foregroun="#a2d9a1") #green
+        self.textbox.tag_config('highlight4', foregroun="#F44747") # red
+        self.textbox.tag_config('highlight5', foregroun="#47f49e") # green
+        
+        self.textbox.insert("0.0", "Download Log:\n", 'info_label')
+        
+        self.textbox.see("end")
+
+        self.textbox.configure(state='disabled')
+        
+    def button_callback(self):
+        # create folder if didn't exist already
+        Path(self.settings_frame.entry_browse.get()).mkdir(parents=True, exist_ok=True)
+
+        # main logic
+        media_type = self.settings_frame.settings.radio_state.get()
+        if (media_type == RADIO_NAMES[0]):
+            media_type = 'video'
+        elif (media_type == RADIO_NAMES[1]):
+            media_type = 'audio'
+        elif (media_type == RADIO_NAMES[2]):
+            media_type = 'both'
+        url = self.settings_frame.entry_field.get()
+        dl_dir = self.settings_frame.entry_browse.get()
+
+        self.gui_disable()
+
+        if url != '':
+            BerriedDL.main(media_type, url, dl_dir, self.textbox)
+
+        self.gui_enable()
+        
+
+    def button_callback_threaded(self):
+        t1 = threading.Thread(target=self.button_callback)
+        t1.start()
+
+    def gui_disable(self):
+        self.button.configure(state='disabled')
+        for radio in self.settings_frame.radiobuttons:
+            radio.configure(state='disabled')
+
+    def gui_enable(self):
+        self.button.configure(state='normal')
+        for radio in self.settings_frame.radiobuttons:
+            radio.configure(state='normal')
+
+
+# ---------------------------------------------------------------------------- #
+#                                      Run                                     #
+# ---------------------------------------------------------------------------- #
+app = App()
+
+def window_exit():
+    # Save settings
+    with open(os.path.join(PATH_CONFIG, "settings.json"), "w") as file:
+        app.settings_frame.settings.dlpath = app.settings_frame.entry_browse.get()
+        app.settings_frame.settings.preJSON()
+        json.dump(jsonpickle.encode(app.settings_frame.settings), file)
+
+    # Close app
+    app.destroy()
+app.protocol("WM_DELETE_WINDOW", window_exit)
+
+app.mainloop()
